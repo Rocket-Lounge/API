d19a1b6b3bf96ceb7843e2fe52b78b8f789aa5d2
Dan Lindsey <dan@mdlindsey.com>
2022-12-28 02:24:21 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.get('/health', (_,res) => res.send('ok'))
+
+const server = http.createServer(app)
+const io = socketio(server)
+// will fire for every new websocket connection
+io.on('connection', socket => {
+  console.info(`<${socket.id}> connected`)
+  socket.on('disconnect', () => console.info(`<${socket.id}> disconnected`))
+  socket.on('self', (slug, ...payload) => {
+    console.log(slug, ...payload)
+    // lookup slug and reconcile to username for steam people?
+    io.emit('player', 'CatFacts', ...payload)
+  })
+})
+
+// important! must listen from `server` not `app`
+server.listen(PORT, () => console.log(`API listening on port ${PORT}!`))
+
+
+
+
