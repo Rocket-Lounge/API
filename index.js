fce49067d4bdef509d6cb12e80033cb7ca43fc56
Dan Lindsey <dan@mdlindsey.com>
2022-12-29 20:24:10 -0500

    Replay tick rate

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 app.get('/health', (_,res) => res.send('ok'))
-app.get('/mock/record/:platform/:userId', (req,res) => {
-  const { platform, userId } = req.params
+app.get('/mock/record/:platform/:userId/:tickRate', (req,res) => {
+  const { platform, userId, tickRate } = req.params
   const slug = `${platform}/${userId}`
   if (Mock.IsRecording(slug)) {
     // stop and save
-    Mock.SaveRecording(slug)
+    Mock.SaveRecording(slug, tickRate)
     res.send('saved')
   } else {
     // start recording
@@ -41,41 +41,44 @@ server.listen(PORT, () => console.log(`API listening on port ${PORT}!`))
 class Mock {
   static FilePath = `${__dirname}/mocks`
   static RecordingMap = {}
+  static MockIterators = {}
+  static MockIntervals = {}
+
   static IsRecording(slug) {
     return Boolean(this.RecordingMap[slug])
   }
   static StartRecording(slug) {
     this.RecordingMap[slug] = []
   }
-  static SaveRecording(slug) {
-    fs.writeFileSync(`${this.FilePath}/${Buffer(slug).toString('base64')}.json`, JSON.stringify(this.RecordingMap[slug]))
+  static SaveRecording(slug, tickRate) {
+    const filePath = `${this.FilePath}/${Buffer(slug).toString('base64')}.json`
+    fs.writeFileSync(filePath, JSON.stringify({ tickRate, data: this.RecordingMap[slug] }))
     this.StopRecording(slug)
   }
   static StopRecording(slug) {
+    clearInterval(this.MockIntervals[slug])
+    delete this.MockIntervals[slug]
     this.RecordingMap[slug] = null
   }
   static AllowRecording(payload) {
     if (this.RecordingMap[payload[0]]) this.RecordingMap[payload[0]].push(payload)
   }
 
-  static MockTickRate = 240
-  static MockIterators = {}
-  static MockIntervals = {}
   static PropagateMocks() {
     fs.readdir(this.FilePath, (err, files) => {
       if (err) return console.log('Unable to scan directory: ' + err)
       for(const file of files) {
-        const mockData = require(`${this.FilePath}/${file}`)
         const slug = Buffer(file, 'base64').toString('ascii')
         if (!this.MockIntervals[slug]) {
+          const { tickRate, data } = require(`${this.FilePath}/${file}`)
           this.MockIterators[slug] = 0
           this.MockIntervals[slug] = setInterval(() => {
-            if (this.MockIterators[slug] >= mockData.length) this.MockIterators[slug] = 0
-            const [, displayName, ...datum] = mockData[this.MockIterators[slug]++]
+            if (this.MockIterators[slug] >= data.length) this.MockIterators[slug] = 0
+            const [, displayName, ...datum] = data[this.MockIterators[slug]++]
             const mockPayload = [`ghost/${slug}`, `(Ghost) ${displayName}`, ...datum]
             io.emit('player', ...mockPayload)
             console.log(...mockPayload)
-          }, 1000/this.MockTickRate)
+          }, 1000/tickRate)
         }
       }
     })
